5d99a2cc6b5080d6bc13239f921aae260e682662
tariq-shreem <tariqshreem00@gmail.com>
2023-02-25 16:16:51 +0200

    two

--- modules/auth/controller/auth.controller.js
+++ modules/auth/controller/auth.controller.js
@@ -28,7 +28,7 @@ let refreshToken=await jwt.sign({id:savedUser._id},process.env.refreshTokenEmail
             `;
 
             let messageRefresh = `
-            <a href="${req.protocol}://${req.headers.host}${process.env.BASEURL}/auth/rftoken/${refreshToken}">Resend verify email</a>
+            <a href="${req.protocol}://${req.headers.host}/${process.env.BASEURL}/auth/rftoken/${refreshToken}">Resend verify email</a>
         `;
           let emaild= await sendEamil(email,'confirm Email',`${message} <br /> ${messageRefresh}`);
           res.status(201).json({messgae:"success"});
