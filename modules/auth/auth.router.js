6393f9f808118d5df1007c22e1e6a65e346f7fde
tariq-shreem <82154326+tariq-shreem@users.noreply.github.com>
2023-03-04 15:25:58 +0200

    Update auth.router.js

--- modules/auth/auth.router.js
+++ modules/auth/auth.router.js
@@ -9,8 +9,8 @@ router.get('/confirmEmail/:token',authController.confirmEmail);
 router.get('/rftoken/:token',authController.refreshtoken);
 
 router.post('/signin',validation(authValidation.signin),authController.signin)
-router.get('/sendCode',authController.sendCode)
-router.get('/forgetPassword',authController.forgetPassword);
+router.patch('/sendCode',authController.sendCode)
+router.patch('/forgetPassword',authController.forgetPassword);
 
 router.get('/qrcode_allUsers',authController.qrcodeAllUser);
 router.get('/getAllUsers',authController.getAllUsers)
